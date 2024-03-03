94b4fa2fe8e04bf27f457afd79dfe9574356da23
revoly afanasev <rewoly@mail.ru>
2024-03-03 11:47:30 +0900

    for student

---
+++ Client/src/main/java/com/example/client/SignController.java
@@ -0,0 +1,23 @@
+package com.example.client;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class SignController {
+    @FXML
+    private TextField loginField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private Button buttonOk;
+
+    @FXML
+    private void setButtonOk() {
+
+    }
+}
